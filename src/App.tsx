f25e2aa3f98f0913fc61e609ea9d2e874574d37b
Andrew Stephens <asteph11@oldwestbury.edu>
2023-04-10 22:49:09 -0400

    Init Vite project files.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, {Fragment} from "react";
+
+function App() {
+    return (
+        <Fragment>
+            <label>This is the beginning of a Vite project.</label>
+        </Fragment>
+    )
+}
+
+export default App;
