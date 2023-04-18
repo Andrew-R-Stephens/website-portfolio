cfced702023351103df81f09c88579fbdabaae92
Andrew Stephens <asteph11@oldwestbury.edu>
2023-04-17 20:42:27 -0400

    + Added hero links and animations

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,13 +1,13 @@
 import React, {Fragment, useEffect, useRef, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import './App.css'
+import {render} from "react-dom";
 
 function Home() {
 
     //const navigate = useNavigate()
 
     const [isEntered, setIsEntered] = useState(false);
-
 
     function onEnterClick(event:any) {
         event.preventDefault()
@@ -19,12 +19,7 @@ function Home() {
         setIsEntered(true);
     }
 
-    useEffect(() =>{
-        const squareFall = document.getElementsByClassName('square-fall-1').item(0)
-        squareFall?.addEventListener("animationend", (event) => {
-            goToGreetingAnchor();
-        });
-    })
+
 
     function goToGreetingAnchor() {
         const greetingAnchor = document.getElementById('greeting-anchor')
@@ -34,12 +29,29 @@ function Home() {
         });
     }
 
+    useEffect(() =>{
+        const documentHeight = () => {
+            const doc = document.documentElement
+            doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+        }
+        window.addEventListener('resize', documentHeight)
+        documentHeight()
+
+        const squareFall = document.getElementsByClassName('square-fall-1').item(0)
+        squareFall?.addEventListener("animationend", (event) => {
+            goToGreetingAnchor();
+        });
+
+    }, [])
+
     return (
         <Fragment>
             <div className={'page-head'}>
                 <div style={{position: "absolute", top:0, width:"100%", height: "100svh", zIndex: 0, overflow: "hidden"}}>
-                    {/*<canvas id={'head-canvas'} width={"100%"} height={"100%"}
-                            style={{display: "block", margin:"auto", width:"100%", height:"100%"}}/>*/}
+                    {/*
+                    <canvas id={'head-canvas'} width={"100%"} height={"100%"}
+                            style={{display: "block", margin:"auto", width:"100%", height:"100%"}}/>
+                    */}
                     <div id={'square-fall'} className={'square-wrapper square-fall-1'}
                          role={isEntered? 'active-square-fall' : ''}>
                         <div className={'square-wrapper'}
@@ -60,41 +72,65 @@ function Home() {
                         <div className={'square-wrapper'}
                              id={'square-4'}/>
                     </div>
-{/*
-
+                    {/*
                     <div id={'square-fall'} className={'square-wrapper square-fall-5'}
                          role={isEntered? 'active-square-fall' : ''}>
                         <div className={'square-wrapper'}
                              id={'square-1'}/>
                     </div>
-*/}
+                    */}
+                    {/*<div className={'gradient-wrapper'}>
+                        <div className="gradient"></div>
+                    </div>*/}
 
                 </div>
                 <div className={'head-header'}>
                     <div className={'header-links'}>
                         <div style={{marginLeft:"3vmin", marginTop: 32}}>
                             <ul>
-                                <li>About</li>
-                                <li>Projects</li>
-                                <li>Curriculum Vitae</li>
-                                <li>Contact</li>
+                                <div className={'hero-link-wrapper'}>
+                                    <li className={'hero-link'}>
+                                        About
+                                    </li>
+                                    <div id={'hero-link-square-wrapper'}>
+                                        <div id={'hero-link-square'}></div>
+                                    </div>
+                                </div>
+                                <div className={'hero-link-wrapper'}>
+                                    <li className={'hero-link'}>Projects</li>
+                                    <div id={'hero-link-square-wrapper'}>
+                                        <div id={'hero-link-square'}></div>
+                                    </div>
+                                </div>
+                                <div className={'hero-link-wrapper'}>
+                                    <li className={'hero-link'}>Curriculum Vitae</li>
+                                    <div id={'hero-link-square-wrapper'}>
+                                        <div id={'hero-link-square'}></div>
+                                    </div>
+                                </div>
+                                <div className={'hero-link-wrapper'}>
+                                    <li className={'hero-link'}>Contact</li>
+                                    <div id={'hero-link-square-wrapper'}>
+                                        <div id={'hero-link-square'}></div>
+                                    </div>
+                                </div>
                             </ul>
                         </div>
                     </div>
                     <div className={'header-label'}>
-                        <div className={'label-container'}
-                             style={{opacity: 0, animation:"fade-in 1s forwards",
-                                 animationDelay: "200ms", paddingBottom:32}}>
-                            <label className={'simple-label'} style={{fontFamily:"TTSSevers-Medium"}}>Hello, my name is Andrew Stephens</label>
+                        <div className={'label-container transition-fade-in'}
+                             style={{animationDuration: "1s", animationDelay: "200ms", paddingBottom:32}}>
+                            <label className={'simple-label'} style={{fontFamily:"TTSSevers-Medium"}}>Hello, my name is </label>
+                            <label className={'simple-label'} style={{fontFamily:"Oldtimer", color: "blueviolet"}}>Andrew Stephens</label>
                         </div>
                     </div>
                     <div className={'header-label'}>
                         <div className={'label-container transition-fade-in'}
-                             style={{opacity: 0, animation:"fade-in 400ms forwards", animationDelay: "1s"}}>
-                            <label className={'simple-label reduced-font'}>Full-stack Web Developer</label>
+                             style={{animationDelay: "1s"}}>
+                            <label className={'simple-label reduced-font'}>Full-stack Developer</label>
                         </div>
                         <div className={'label-container transition-fade-in'}
-                             style={{opacity: 0, animation:"fade-in 400ms forwards", animationDelay: "1300ms"}}>
+                             style={{animationDelay: "1300ms"}}>
                             <label className={'simple-label reduced-font'}>Native Android Engineer</label>
                         </div>
                     </div>
@@ -112,7 +148,7 @@ function Home() {
                     </ul>
                 </div>*/}
             </div>
-            <div className={'page-body'}>
+            <div className={'page-body'} style={{background: "snow"}}>
                 <div id={'greeting-anchor'} className={'greeting'}>
                     <label>Hello from label</label>
                 </div>
